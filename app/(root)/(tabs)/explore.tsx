841bd1393add58570fe23939c2b7e7e55213f54a
Olivier <olivier@MacBook-Pro-de-Olivier.local>
2026-04-01 19:06:30 +0200

    explore page done

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,98 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Card } from '@/components/Cards';
+import Filters from '@/components/Filters';
+import NoResults from '@/components/NoResults';
+import Search from '@/components/Search';
+import icons from '@/constants/icons';
+import { getProperties } from '@/lib/appwrite';
+import { useAppwrite } from '@/lib/useAppwrite';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const explore = () => {
+export default function Explore() {
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+  const router = useRouter();
+
+  // properties
+  const {
+    data: properties,
+    loading,
+    refetch,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter ?? 'All',
+      query: params.query ?? '',
+      limit: 20,
+    },
+    skip: true,
+  });
+
+  const handleCardPress = (id: string) => {
+    router.push(`/properties/${id}`);
+  };
+
+  // refetch when params change
+  useEffect(() => {
+    refetch({
+      filter: params.filter ?? 'All',
+      query: params.query ?? '',
+      limit: 20,
+    });
+  }, [params.filter, params.query]);
+
   return (
-    <View>
-      <Text>explore</Text>
-    </View>
-  )
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={properties ?? []}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : (
+            <NoResults />
+          )
+        }
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <TouchableOpacity
+                onPress={() => router.back()}
+                className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center"
+              >
+                {' '}
+                <Image source={icons.backArrow} className="size-9" />
+              </TouchableOpacity>
+              <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">
+                Search for your ideal
+              </Text>
+              <Image source={icons.bell} className="w-6 h-6" />
+            </View>
+            <Search />
+            <View className="mt-5">
+              <Filters />
+              <Text className="text-xl font-rubik-bold text-black-300 mt-5">
+                Found {properties?.length} Properties
+              </Text>
+            </View>
+          </View>
+        }
+      />
+    </SafeAreaView>
+  );
 }
-
-export default explore
